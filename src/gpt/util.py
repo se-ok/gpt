194c4d0c073ecbd0a5ece911495da78ee20ab15f
se-ok <33045469+se-ok@users.noreply.github.com>
2024-01-26 16:06:30 +0900

    Update util.py

--- src/gpt/util.py
+++ src/gpt/util.py
@@ -1,5 +1,9 @@
+import json
 import os
+import re
+from pathlib import Path
 from pprint import pprint
+from typing import Iterator, TextIO
 
 import requests
 
@@ -15,6 +19,43 @@ def check_openai_limit(model: str = "gpt-3.5-turbo"):
     )
 
     if response.status_code != 200:
-        raise RuntimeError(f"OpenAI API request error with code {response.status_code}, {response.content}")
+        raise RuntimeError(
+            f"OpenAI API request error with code {response.status_code}, {response.content}"
+        )
 
     pprint({key: val for key, val in response.headers.items() if key.startswith("x-ratelimit")})
+
+
+def save_jsonl(objects: Iterator[dict], save_to: str | Path | TextIO):
+    if not all(isinstance(obj, dict) for obj in objects):
+        raise ValueError("Objects to save as NDJSON should all be dict.")
+
+    if isinstance(save_to, TextIO):
+        for obj in objects:
+            save_to.write(json.dumps(obj, ensure_ascii=False) + "\n")
+
+    else:
+        with open(save_to, "w") as f:
+            for obj in objects:
+                print(json.dumps(obj, ensure_ascii=False), file=f)
+
+
+def load_jsonl(path: str | Path | TextIO):
+    if isinstance(path, TextIO):
+        return [json.loads(line) for line in path.readlines()]
+
+    with open(path) as f:
+        return [json.loads(line) for line in f.readlines()]
+
+
+_ptrn_enumeration = re.compile(r"^\d+\.\s*(.*)$")
+
+
+def parse_enumeration(line: str) -> str:
+    """Parse "12. XXXX" for their format and take the enumeration part away."""
+    match = re.match(_ptrn_enumeration, line.strip())
+
+    if match is None:
+        raise ValueError(f"Unable to parse with pattern '1. XXX': {line.strip()}")
+
+    return match.group(1).strip()
